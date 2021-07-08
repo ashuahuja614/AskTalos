288e3de9e550819740c7191ad6ee048b68627d4c
ashishahujaa450 <ahujaashu614@gmail.com>
2021-07-08 23:32:11 +0530

    material module created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-root',
