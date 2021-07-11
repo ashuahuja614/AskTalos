96ca172a74cb6c5d8acbe11f9cafe00e82427180
ashishahujaa450 <ahujaashu614@gmail.com>
2021-07-11 15:38:45 +0530

    auth module creation with routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
   },
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
     path: '',
     redirectTo: 'admin',
     pathMatch: 'full'
