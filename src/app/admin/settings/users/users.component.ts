dc0b987da5a211c10db0f04cea8f4e9dec5ebab5
designblank02 <designblank02@gmail.com>
2021-08-05 22:46:07 +0530

    Settings Popups UI, Create new Team and Add or edit user

--- src/app/admin/settings/users/users.component.ts
+++ src/app/admin/settings/users/users.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddNewUserPopupComponent } from './../add-new-user-popup/add-new-user-popup.component';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  constructor(private _dialog: MatDialog) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  /**
+   * @name onAddNewUSer
+   * @description open add new user popup
+   */
+  public onAddNewUSer() {
+    const dialogRef = this._dialog.open(AddNewUserPopupComponent, {
+      width: '700px',
+      autoFocus: false,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {});
   }
-
 }
